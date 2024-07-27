42dfc3f89e84ec615b34fdacc03e7e8ce0fd33c8
Jackyking1936 <j75772763@gmail.com>
2024-07-28 00:35:13 +0800

    finish layout setting

--- swing_trade.py
+++ swing_trade.py
@@ -19,13 +19,13 @@ class MainApp(QWidget):
         self.setWindowTitle("Python波段單下單機")
         self.resize(1200, 600)
 
-                # 製作上下排列layout上為庫存表，下為log資訊
+        # 製作上下排列layout上為庫存表，下為log資訊
         layout = QVBoxLayout()
 
         layout_table = QHBoxLayout()
         # 庫存表表頭
-        self.old_cur_header = ['股票名稱', '股票代號', '類別', '庫存股數', '庫存均價', '現價', '停損', '停利', '損益試算', '獲利率%']
-        self.new_pos_header = ['股票名稱', '股票代號', '類別', '庫存股數', '庫存均價', '現價', '停損', '停利', '損益試算', '獲利率%']
+        self.old_cur_header = ['股票名稱', '股票代號', '類別', '庫存股數', '庫存均價', '現價', '損益試算', '獲利率%']
+        self.new_pos_header = ['股票名稱', '股票代號', '現價', '委託數量', '委託價格', '成交數量', '成交價格']
         
         self.old_cur_table = QTableWidget(0, len(self.old_cur_header))
         self.old_cur_table.setHorizontalHeaderLabels([f'{item}' for item in self.old_cur_header])
@@ -37,39 +37,66 @@ class MainApp(QWidget):
         layout_table.addWidget(self.new_pos_table)
         
         # 整個設定區layout
-        layout_condition = QGridLayout()
+        layout_parameter = QGridLayout()
 
-        # 監控區layout設定
-        label_monitor = QLabel('預設新部位停損停利設定')
-        label_monitor.setStyleSheet("QLabel { font-size: 24px; font-weight: bold; }")
-        label_monitor.setAlignment(Qt.AlignCenter)
-        layout_condition.addWidget(label_monitor, 0, 0)
-        label_sl = QLabel('\t預設停損(%, 0為不預設停損):')
-        layout_condition.addWidget(label_sl, 1, 0)
-        self.lineEdit_default_sl = QLineEdit()
-        self.lineEdit_default_sl.setText('-5')
-        layout_condition.addWidget(self.lineEdit_default_sl, 1, 1)
-        label_sl_post = QLabel('%')
-        layout_condition.addWidget(label_sl_post, 1, 2)
-        label_tp = QLabel('\t預設停利(%, 0為不預設停損):')
-        layout_condition.addWidget(label_tp, 2, 0)
-        self.lineEdit_default_tp = QLineEdit()
-        self.lineEdit_default_tp.setText('5')
-        layout_condition.addWidget(self.lineEdit_default_tp, 2, 1)
-        label_tp_post = QLabel('%')
-        layout_condition.addWidget(label_tp_post, 2, 2)
+        # 參數設置區layout設定
+        label_input_title = QLabel('配置參數設定')
+        label_input_title.setStyleSheet("QLabel { font-size: 24px; font-weight: bold; }")
+        label_input_title.setAlignment(Qt.AlignCenter)
+        layout_parameter.addWidget(label_input_title, 0, 0)
+
+        label_total_amount = QLabel('配置總金額:')
+        layout_parameter.addWidget(label_total_amount, 1, 0)
+        label_total_amount.setAlignment(Qt.AlignRight)
+        self.lineEdit_default_total_amount = QLineEdit()
+        self.lineEdit_default_total_amount.setText('100')
+        layout_parameter.addWidget(self.lineEdit_default_total_amount, 1, 1)
+        label_total_amount_post = QLabel('萬元, ')
+        layout_parameter.addWidget(label_total_amount_post, 1, 2)
+
+        label_file_path = QLabel('目標清單路徑:')
+        layout_parameter.addWidget(label_file_path, 0, 3)
+        self.lineEdit_default_file_path = QLineEdit()
+        layout_parameter.addWidget(self.lineEdit_default_file_path, 0, 4)
+        folder_btn = QPushButton('')
+        folder_btn.setIcon(QIcon('folder.png'))
+        layout_parameter.addWidget(folder_btn, 0, 5)
+
+        label_est_sell = QLabel('預估賣出金額:')
+        layout_parameter.addWidget(label_est_sell, 1, 3)
+        self.lineEdit_default_est_sell = QLineEdit()
+        self.lineEdit_default_est_sell.setReadOnly(True)
+        layout_parameter.addWidget(self.lineEdit_default_est_sell, 1, 4)
+        label_est_sell_post = QLabel('元')
+        layout_parameter.addWidget(label_est_sell_post, 1, 5)
+
+        label_est_buy = QLabel('預估買進金額:')
+        layout_parameter.addWidget(label_est_buy, 2, 3)
+        self.lineEdit_default_est_buy = QLineEdit()
+        self.lineEdit_default_est_buy.setReadOnly(True)
+        layout_parameter.addWidget(self.lineEdit_default_est_buy, 2, 4)
+        label_est_buy_post = QLabel('元')
+        layout_parameter.addWidget(label_est_buy_post, 2, 5)
+
+        label_est_pnl = QLabel('預估賣出損益:')
+        layout_parameter.addWidget(label_est_pnl, 3, 3)
+        self.lineEdit_default_est_pnl = QLineEdit()
+        self.lineEdit_default_est_pnl.setReadOnly(True)
+        layout_parameter.addWidget(self.lineEdit_default_est_pnl, 3, 4)
+        label_est_pnl_post = QLabel('元')
+        layout_parameter.addWidget(label_est_pnl_post, 3, 5)
 
         # 啟動按鈕
-        self.button_start = QPushButton('開始監控')
+        self.button_start = QPushButton('開始下單')
         self.button_start.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.button_start.setStyleSheet("QPushButton { font-size: 24px; font-weight: bold; }")
-        layout_condition.addWidget(self.button_start, 0, 6, 3, 1)
+        layout_parameter.addWidget(self.button_start, 1, 6, 3, 1)
 
         # 停止按鈕
-        self.button_stop = QPushButton('停止監控')
+        self.button_stop = QPushButton('停止下單')
         self.button_stop.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.button_stop.setStyleSheet("QPushButton { font-size: 24px; font-weight: bold; }")
-        layout_condition.addWidget(self.button_stop, 0, 6, 3, 1)
+        layout_parameter.addWidget(self.button_stop, 1, 6, 3, 1)
         self.button_stop.setVisible(False)
 
         # 模擬區layout設定
@@ -90,12 +117,12 @@ class MainApp(QWidget):
         self.log_text.setReadOnly(True)
 
         layout.addLayout(layout_table)
-        layout.addLayout(layout_condition)
+        layout.addLayout(layout_parameter)
         layout.addLayout(layout_sim)
         layout.addWidget(self.log_text)
 
-        layout.setStretchFactor(layout_table, 6)
-        layout.setStretchFactor(layout_condition, 2)
+        layout.setStretchFactor(layout_table, 7)
+        layout.setStretchFactor(layout_parameter, 1)
         layout.setStretchFactor(layout_sim, 1)
         layout.setStretchFactor(self.log_text, 3)
 
